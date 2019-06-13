c60465a8ea777e02c3f06f90bd362e65045b675e
duxiangyu <duxiangyu>
2019-06-13 18:23:23 +0800

    修改启动类名

--- xiaoyuan/src/main/java/com/xiaoyuan/XiaoyuanApplication.java
+++ xiaoyuan/src/main/java/com/xiaoyuan/XiaoyuanApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XiaoyuanMApplication {
+public class XiaoyuanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(XiaoyuanMApplication.class, args);
+        SpringApplication.run(XiaoyuanApplication.class, args);
     }
 
 }
--- xiaoyuan/src/test/java/com/xiaoyuan/XiaoyuanApplicationTests.java
+++ xiaoyuan/src/test/java/com/xiaoyuan/XiaoyuanApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class XiaoyuanMApplicationTests {
+public class XiaoyuanApplicationTests {
 
     @Test
     public void contextLoads() {
